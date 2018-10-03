5debe8248ac4e90dae660cdb23f318254cc639d3
diodonfrost <keres@protonmail.com>
2018-10-03 10:21:35 +0200

    add crontab package requirement

--- tests/inspec/install_spec.rb
+++ tests/inspec/install_spec.rb
@@ -2,6 +2,7 @@
 
 logrotate_package = 'logrotate'
 logrotate_crontab_job = '/usr/sbin/logrotate /etc/logrotate.conf'
+logroate_configfile = '/etc/logrotate.conf'
 
 control 'install-01' do
   impact 1.0
@@ -28,3 +29,17 @@ end
 control 'install-03' do
   impact 1.0
   title 'Logrotate file'
+  desc 'Logrotate file shoud be only readable and writable by root'
+    describe file(logroate_configfile) do
+      it { should exist }
+      it { should be_file }
+      it { should be_owned_by 'root' }
+      it { should be_writable.by('owner') }
+      it { should_not be_executable }
+      it { should_not be_readable.by('group') }
+      it { should_not be_readable.by('other') }
+      it { should be_writable.by('owner') }
+      it { should_not be_writable.by('group') }
+      it { should_not be_writable.by('other') }
+    end
+  end
